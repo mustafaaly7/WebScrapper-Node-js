1a817a771e310616a16a1f7307184d9e14422212
Mustafa Ali <mustafaaaly27@gmail.com>
2025-01-27 20:24:00 +0500

    getting products name and prices and adding it into single object with keys

--- utils/scrapper.js
+++ utils/scrapper.js
@@ -2,33 +2,49 @@ import axios from "axios";
 // import { Cheerio } from "cheerio";
 import * as cheerio from 'cheerio';
 
-const url1 ="https://dealsmall.pk/"
+const url1 = "https://dealsmall.pk/"
 
-let productNames =[]
-let productPrices=[]
+let productNames = []
+let productPrices = []
 
 
-const fetchUrl =async(url)=>{
-const results = await axios.get(url)
-// console.log("results", results.te);
+const fetchUrl = async (url) => {
+    const results = await axios.get(url)
+    // console.log("results", results.te);
 
-return cheerio.load(results.data)
+    return cheerio.load(results.data)
 }
 
-const getResults= async()=>{
-const $ = await fetchUrl(url1)
-// console.log("$",$.text());
+const getResults = async () => {
+    const $ = await fetchUrl(url1)
+    // console.log("$",$.text());
 
-$(".grid-product__title").each((index,element)=>{
-    productNames.push($(element).text())
+//defining the .classname and pushing it into our empty array using Jquery 
+    $(".grid-product__title").each((index, element) => {
+        productNames.push($(element).text())
+
+    })
+
+    //here we targeted with classname and html tag span using Jquery
+    $(".price-regular span").each((index, element) => {
+        productPrices.push($(element).text())
+
+    })
+
+    let merged = {}
     
-})
-$(".money").each((index,element)=>{
-productPrices.push($(element).text())
+    // productNames.forEach((names, i) => merged[names] = productPrices[   i   ])
 
-})
-console.log("PRODUCT NAMES => ", productNames);
-console.log("Prices =>" , productPrices);
+    const minLength = Math.min(productNames.length, productPrices.length); // to make one condition if we use operator it'll make it a truthy value
+
+    //condtion to iterate both arrays and integrate into the single object with keys 
+    for (let i = 0; i <= minLength; i++) {
+        merged[`Product${i}`] = productNames[i]
+        merged[`Price$${i}`] = productPrices[i]
+    }
+
+
+    console.log("Merged Data =>", merged);
 
 
 }
